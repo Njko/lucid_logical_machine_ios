641b5dec4b3fc05f8c56fd69cc22e69c1e57ca82
Nicolas LINARD <nicolas.linard@gmail.com>
2026-08-25 08:58:29 +0200

    Initial Commit

---
+++ LucidLogicalMachineDemo/LucidLogicalMachineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucidLogicalMachineDemoApp.swift
+//  LucidLogicalMachineDemo
+//
+//  Created by A422GQ on 25/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct LucidLogicalMachineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
